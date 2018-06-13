d9d76605aa0147b5595fe2d3271a70aaa30ef0a4
Chris Evans <evanschris@users.noreply.github.com>
2018-06-13 11:46:54 +0100

    sort sub pages basic content

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   $('#not_successful').click(function(){
 
     // IN REALITY - this would only be used for mobile, we would want an if statement here that pulls up a better designed widget for desktop
-    var not_successful = prompt("Unsuccessful contact with " + $('#meeting_family_name').html(), "Add a note...");
+    var not_successful = prompt("Unsuccessful contact with " + $('#visit_family_name').html(), "Add a note...");
     var today = new Date();
     var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
     var time = today.getHours() + ":" + ('0' + today.getMinutes()).slice(-2);
@@ -14,10 +14,12 @@ $(document).ready(function() {
 
     console.log(not_successful)
 
-    if (not_successful != null && not_successful != "Add a note...") {
-      $('#meeting_contact').html('Meeting recorded unsuccessful at ' + dateTime + " - <strong>" + not_successful + "</strong>");
-    } else {
-      $('#meeting_contact').html('Meeting recorded unsuccessful at ' + dateTime);
+    if (not_successful != null) {
+      if (not_successful != "Add a note..." || not_successful != "") {
+        $('#visit_contact').html('Visit recorded unsuccessful at ' + dateTime + " - <strong>" + not_successful + "</strong>");
+      } else {
+        $('#visit_contact').html('Visit recorded unsuccessful at ' + dateTime);
+      }
     }
   });
 });
