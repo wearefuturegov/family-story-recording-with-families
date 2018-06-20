1c1c9b947e98178af44df8217c95142a132a2d69
Chris Evans <evanschris@users.noreply.github.com>
2018-06-20 15:59:28 +0100

    add in multiple save buttons and restyle 2nd buttons

--- app/assets/javascripts/display_page.js
+++ app/assets/javascripts/display_page.js
@@ -4,7 +4,6 @@ $.fn.displayPage = function() {
   var visit_notes = localStorage.getItem("visit_notes");
   if(visit_notes) {
     $('#visit_notes_saved').html(visit_notes);
-    //localStorage.removeItem("visit_notes");
   }
 
   var agreed_actions = localStorage.getItem("agreed_actions");
@@ -21,7 +20,6 @@ $.fn.displayPage = function() {
       $('#agreed_actions_saved').append(action);
     });
   }
-  //localStorage.removeItem("agreed_actions");
 
 
   $('.location_choice .content').click(function() {
@@ -32,6 +30,11 @@ $.fn.displayPage = function() {
 
   $('.single_person .delete').click(function() {
     $(this).closest('.single_person').slideUp();
+  });
+
+  $('#share_visit').click(function() {
+    localStorage.removeItem("visit_notes");
+    localStorage.removeItem("agreed_actions");
   });
 };
 
